9764eef39514e629700dded59783c8ccc3ac3309
david ruby <everestso@aol.com>
2013-08-18 16:52:18 -0700

    clean up

--- python/ds.1.py
+++ python/ds.1.py
@@ -1,20 +1,28 @@
 import urllib2
 import json
 
-url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
-       'v=1.0&q=thomas%20bayes&userip=INSERT-USER-IP')
+def test1():
+   url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
+          'v=1.0&q=thomas%20bayes&userip=INSERT-USER-IP')
 
-request = urllib2.Request(url, None, {'Referer': 'www.INSERT-USER-SITE.com'})
-response = urllib2.urlopen(request)
+   request = urllib2.Request(url, None, {'Referer': 'www.INSERT-USER-SITE.com'})
+   response = urllib2.urlopen(request)
 
-# Process the JSON string.
-results = json.load(response)
-# now have some fun with the results...
+   # Process the JSON string.
+   results = json.load(response)
+   # now have some fun with the results...
 
-y = results['responseData']['results']
+   y = results['responseData']['results']
 
-for i in y:
-    print i['url']
+   for i in y:
+      print i['url']
 
+def test2():
+   url = "http://www.amazon.com/First-Course-Database-Systems-Edition/dp/013600637X"
 
+   request = urllib2.Request(url, None, {'Referer': 'www.INSERT-USER-SITE.com'})
+   response = urllib2.urlopen(request)
 
+   results = response.read()
+   print results
+   
