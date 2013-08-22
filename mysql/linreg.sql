606d0e4baef289bd2f89777010a4ba1aa81d6bd6
David C. Ruby <everestso@aol.com>
2013-08-22 11:22:48 -0700

    Iris content

--- mysql/linreg.sql
+++ mysql/linreg.sql
@@ -9,6 +9,20 @@ select sum(High2) from
  (select power(High,2) as High2 from songs limit 50000) x
 
 select sum(CH*High) as XY from songs limit 50000
-
+ 
 select count(CH) from songs
 select * from r1
+
+create table iris_2d (x float, y float, class varchar(30));
+
+LOAD DATA 
+LOCAL INFILE 'D:/Documents/GitHub/CSci226.Fall13/assignments/iris/iris.2D.csv' 
+INTO TABLE iris_2d
+FIELDS TERMINATED BY ',';
+
+select * from iris_2d;
+
+select x,y from iris_2D where class = "Iris-setosa"
+select * from iris_2D where class like "Iris-setosa%"
+delete from iris_2d;
+drop table iris_2d;
