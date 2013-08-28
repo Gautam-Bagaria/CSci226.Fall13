bb34eb282867939c49f1ac53b47996a2a82da39e
David C. Ruby <everestso@aol.com>
2013-08-28 11:10:17 -0700

    StudentDB Updates

--- assignments/studentDB/StudentDB.sql
+++ assignments/studentDB/StudentDB.sql
@@ -15,7 +15,15 @@ select * from StudentDB where semester = 'Fall'
 select * from StudentDB where semester = 'Fall' and year=2013
 select 'Fall', '2013', count(*) from StudentDB where semester = 'Fall' and year=2013
 
+select grade from StudentDB where grade like 'B%'
+select grade from StudentDB where grade = 'B'
+select * from StudentDB where grade = 'B'
+select count(*) from StudentDB where grade ='B'
+
 LOAD DATA 
 LOCAL INFILE 'D:/Documents/GitHub/CSci226.Fall13/assignments/StudentDB/StudentDB.csci226.csv' 
 INTO TABLE StudentDB
 FIELDS TERMINATED BY ',';
+
+update StudentDB set grade = 'B' where grade like 'B%'
+select * from StudentDB where grade like 'C%' and grade != 'CR'
